d82f2639955a47579c00735a514103b314ae9e19
Eduardo Urbaez <eurbaezjr@gmail.com>
2020-03-03 00:33:13 -0800

    Updates

---
+++ precache-manifest.bc4a1aeb3264a18f59c1d61138b06c1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae5a688b5510203f4a710df049d8426b",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "a87686b6b75896364f4c",
+    "url": "/react-employee-directory/static/css/main.2d9bc9b9.chunk.css"
+  },
+  {
+    "revision": "51ecb50840cb403abf72",
+    "url": "/react-employee-directory/static/js/2.cf155f9f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-employee-directory/static/js/2.cf155f9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a87686b6b75896364f4c",
+    "url": "/react-employee-directory/static/js/main.9562ad6c.chunk.js"
+  },
+  {
+    "revision": "82a5ab163b64b5d8b7e7",
+    "url": "/react-employee-directory/static/js/runtime-main.c15c2ac1.js"
+  }
+]);
